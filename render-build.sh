2878ac1fe3e956bb8f81b5a46d93fa5517c1c5dc
minhazul5000 <mdminhazulislampavel@gmail.com>
2025-04-26 15:42:18 +0600

    render build fix

--- render-build.sh
+++ render-build.sh
@@ -19,7 +19,7 @@ if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
 echo "...Copying Puppeteer Cache from Build Cache"
 # Copying from the actual path where Puppeteer stores its Chrome binary
 cp -R /opt/render/.cache/puppeteer/chrome/linux-135.0.7049.114/chrome-linux64/chrome $PUPPETEER_CACHE_DIR
-else
-echo "...Storing Puppeteer Cache in Build Cache"
-cp -R $PUPPETEER_CACHE_DIR /opt/render/.cache/puppeteer/chrome/linux-135.0.7049.114/chrome-linux64/chrome
+#else
+#echo "...Storing Puppeteer Cache in Build Cache"
+#cp -R $PUPPETEER_CACHE_DIR /opt/render/.cache/puppeteer/chrome/linux-135.0.7049.114/chrome-linux64/chrome
 fi
